6f244010a825da8d97cfc9412faae20309056bae
OGSteinglass <114182921+OGSteinglass@users.noreply.github.com>
2026-03-11 15:54:21 -0700

    key input change, added a room array with no connections
    to add new inputs, rather than taking the space bar, use: project- project settings - input

--- wampus-game/Scripts/map.gd
+++ wampus-game/Scripts/map.gd
@@ -1,3 +1,27 @@
 extends Node2D
 
-var roomlist
+#list of collumns of rooms
+var roomList = []
+
+
+func fillrooms():
+	#add rooms to array
+	for i in range(6):
+		#create a collumn
+		roomList.append([])
+		#fill in collumn
+		for j in range(5):
+			var room = load("res://Scenes/room.tscn")
+			roomList[i].append(room)
+	
+	#fully connect rooms
+	for i in range(6):
+		for j in range(5):
+			roomList[i][j].setAdjacents([])
+	
+	
+	#PLACEHOLDER randomly delete connections
+	
+	
+		
+		
