67887db5476005979b98112e32a216b825dee000
AtalayKT <atalaykerim@outlook.com>
2026-03-12 15:52:39 -0700

    setting up basic riddle logic and 3 example riddles

--- wampus-game/Scripts/pit.gd
+++ wampus-game/Scripts/pit.gd
@@ -6,6 +6,12 @@ extends Control
 @onready var answers = $Answers
 @onready var player = $"../Character"
 @onready var timer = $"../Timer"
+var step = 0
+var riddleList = []
+var riddle1 = ["What has roots that nobody sees, is taller than trees, up, up it goes, and yet never grows?", "A tall tree.", "A mountain", "A shadow.", "The Parthenon."]
+var riddle2 = ["There is a right triangle with sides A, B, and C. Side AB is 5 feet, angle A is 45, and angle B is 90. What is the area of the triangle?", "56 feet squared.", "25 feet squared.", "12.5 feet squared.", "25.5 feet squared."]
+var riddle3 = ["Idk man.", "56 feet squared.", "25 feet squared.", "12.5 feet squared.", "25.5 feet squared."]
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,19 +20,26 @@ func _ready() -> void:
 	announcement2.hide()
 	question.hide()
 	answers.hide()
+	riddleList = [riddle1, riddle2, riddle3]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player.position.y > 700:
+	if player.position.y > 700 and step == 0:
 		announcement.show()
 		announcement2.show()
 		timer.start()
-	
+		step += 1
+
 
 
 func _on_timer_timeout() -> void:
+	print("this works")
 	announcement.hide()
 	announcement2.hide()
 	question.show()
 	answers.show()
+	var riddleChoice = randi_range(0,2)
+	print(riddleChoice)
+	$Question.text = (riddleList[riddleChoice])[0]
+	$Answers.text = "A: " + (riddleList[riddleChoice])[1] + "\nB: " + (riddleList[riddleChoice])[2] + "\nC: " + (riddleList[riddleChoice])[3] + "\nD: " + (riddleList[riddleChoice])[4]
--- wampus-game/Scripts/riddles.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-var riddle = [question, answer1, answer2, answer3, answer4]
-var riddleList = []
