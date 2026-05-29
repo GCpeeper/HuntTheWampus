7642430ada44c17d68890fbb8c4c8efa9272772d
AtalayKT <atalaykerim@outlook.com>
2026-05-29 13:55:45 -0700

    saving and loading stuff

--- wampus-game/Scripts/main_menu.gd
+++ wampus-game/Scripts/main_menu.gd
@@ -1,5 +1,7 @@
 extends CenterContainer
 
+func _ready() -> void:
+	Global._loadScores()
 
 func _on_new_game_pressed() -> void:
 	$LineEdit.visible = true
@@ -13,3 +15,10 @@ func _on_high_scores_pressed() -> void:
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	Global.username = new_text
 	get_tree().change_scene_to_file("res://Scenes/map.tscn")
+
+
+func _on_load_game_pressed() -> void:
+	Global._loadScores()
+
+func _on_save_game_pressed() -> void:
+	Global._saveScores()
